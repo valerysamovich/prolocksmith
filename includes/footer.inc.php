7069124542aec13c9d7f9fcd62e646541c91a883
Samovich <valerysamovich@gmail.com>
2014-12-21 23:49:32 -0500

    Fiw the tabbing and whitespace

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,18 +1,18 @@
 <!-- Footer element -->
 <div id="footer">
-	<div id="footer-nav">
-	<ul>
-		<li><a href="index.php?pagelet=index"><?php echo constant("HOME"); ?></a><span style="color:rgb(239,161, 21);">&nbsp;|&nbsp;</span></li>
-		<li><a href="index.php?pagelet=about"><?php echo constant("ABOUT"); ?></a><span style="color:rgb(239,161, 21);">&nbsp;|&nbsp;</span></li>
-		<li><a href="index.php?pagelet=services"><?php echo constant("SERVICES"); ?></a><span style="color:rgb(239,161, 21);">&nbsp;|&nbsp;</span></li>
-		<li><a href="index.php?pagelet=contact"><?php echo constant("CONTACT"); ?></a></li>
-	</ul>
+    <div id="footer-nav">
+    <ul>
+        <li><a href="index.php?pagelet=index"><?php echo constant("HOME"); ?></a><span style="color:rgb(239,161, 21);">&nbsp;|&nbsp;</span></li>
+        <li><a href="index.php?pagelet=about"><?php echo constant("ABOUT"); ?></a><span style="color:rgb(239,161, 21);">&nbsp;|&nbsp;</span></li>
+        <li><a href="index.php?pagelet=services"><?php echo constant("SERVICES"); ?></a><span style="color:rgb(239,161, 21);">&nbsp;|&nbsp;</span></li>
+        <li><a href="index.php?pagelet=contact"><?php echo constant("CONTACT"); ?></a></li>
+    </ul>
     </div>
-	<p id="footer-sign">Prolocksmithfl App &copy; 2013 | Valery Y. Samovich</p>
-	<!--
+    <p id="footer-sign">Prolocksmithfl App &copy; 2013 | Valery Y. Samovich</p>
+    <!--
     <span id="validation"> <a href="http://validator.w3.org/check?uri=referer">
-	<img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1!" height="31" width="88" /></a>
-	<a href="http://jigsaw.w3.org/css-validator/check/referer"> <img height="31" width="88" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!"/></a></span>
+    <img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1!" height="31" width="88" /></a>
+    <a href="http://jigsaw.w3.org/css-validator/check/referer"> <img height="31" width="88" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!"/></a></span>
     -->
 </div>
 <!-- End of footer -->
--- scripts/slideshowgallery.js
+++ scripts/slideshowgallery.js
@@ -5,18 +5,18 @@
 ***********************************************/
 
 var mygallery=new fadeSlideShow({
-	wrapperid: "fadeshow1", //ID of blank DIV on page to house Slideshow
-	dimensions: [900, 150], //width/height of gallery in pixels. Should reflect dimensions of largest image
-	imagearray: [
-	["images/banner-1.png"],
-	["images/banner-2.png"],
-	["images/banner-3.png"],
-	["images/banner-4.png"],
-	["images/banner-5.png"], //<--no trailing comma after very last image element!
-	],
-	displaymode: {type:'auto', pause:2500, cycles:0, wraparound:false},
-	persist: false, //remember last viewed slide and recall within same session?
-	fadeduration: 500, //transition duration (milliseconds)
-	descreveal: "ondemand",
-	togglerid: ""
+    wrapperid: "fadeshow1", //ID of blank DIV on page to house Slideshow
+    dimensions: [900, 150], //width/height of gallery in pixels. Should reflect dimensions of largest image
+    imagearray: [
+    ["images/banner-1.png"],
+    ["images/banner-2.png"],
+    ["images/banner-3.png"],
+    ["images/banner-4.png"],
+    ["images/banner-5.png"], //<--no trailing comma after very last image element!
+    ],
+    displaymode: {type:'auto', pause:2500, cycles:0, wraparound:false},
+    persist: false, //remember last viewed slide and recall within same session?
+    fadeduration: 500, //transition duration (milliseconds)
+    descreveal: "ondemand",
+    togglerid: ""
 })
