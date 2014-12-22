d0a7cca03d63d61462f1e7246b3ee2a691296da4
Samovich <valerysamovich@gmail.com>
2014-12-22 00:07:48 -0500

    Fiw the tabbing and whitespace

--- pagelets/about.inc.php
+++ pagelets/about.inc.php
@@ -1,20 +1,20 @@
 <!-- Content element -->
 <div id="content-about"> 
-	<!-- Primary element -->
-	<div id="primarycontent">
-		<p id="content-header"><?php echo constant("ABOUT_HEADER"); ?></p>
+    <!-- Primary element -->
+    <div id="primarycontent">
+        <p id="content-header"><?php echo constant("ABOUT_HEADER"); ?></p>
         <p id="content-text">PROLOCKSMITH has provided thousands of locks and lock/security systems of every make and grade conceivable to our Orlando LOCKS &amp; LOCKSMITHS clients.  Moreover, our locksmiths treat our clients as valued business partners. That is, we treat all of our clients as equals and value their input so that the end result satisfies both parties. Not everyone needs the latest-and-greatest propriety technology to protect their bicycle or moped. We will help you find the ideal locking system to meet your unique Orlando LOCKS &amp; LOCKSMITHS needs without breaking the bank. To do so, we will arrange a free consultation as well as a free inspection of the vicinity or object that needs protecting to devise a custom security plan for you. Moreover, we currently provide lock picking, forging and installation services for the industrial, residential, forensic, and transport sectors.</p>
         <p id="content-text">CALL US NOW AT (407) 319-8304</p>
-	</div>
-	<!-- End of primary -->
-	<!-- Left column element -->
-	<div id="rightcolumn">
-		<p id="column-header">24/7 EMERGENCY SERVICE</p>
-		<p id="column-text">CALL NOW at 407-319-8304 for a 24/7 Emergency Access Services and free consultation.</p>
-		<p id="column-image"><img src="images/emergency.jpg" width="180" height="180" alt="Locksmith 24/7 Emergency Service" /></p>
-		<p id="column-footer">LOCKSMITH SPECIALIST</p>
+    </div>
+    <!-- End of primary -->
+    <!-- Left column element -->
+    <div id="rightcolumn">
+        <p id="column-header">24/7 EMERGENCY SERVICE</p>
+        <p id="column-text">CALL NOW at 407-319-8304 for a 24/7 Emergency Access Services and free consultation.</p>
+        <p id="column-image"><img src="images/emergency.jpg" width="180" height="180" alt="Locksmith 24/7 Emergency Service" /></p>
+        <p id="column-footer">LOCKSMITH SPECIALIST</p>
         <p id="column-text">Andy Umansky is experienced locksmith with many years of experience helping clients in Central Florrida area.</p>
-	</div>
-	<!-- End of Left column -->
+    </div>
+    <!-- End of Left column -->
 </div>
 <!-- End of content -->
